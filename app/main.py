22e298b7c361797a914880e38b25f2f9e482762f
Ronnie Cordova <rcordova1801@gmail.com>
2026-02-17 19:17:32 -0500

    fix: enable CORS and optimize docker build for v1.0.4

--- app/main.py
+++ app/main.py
@@ -7,11 +7,20 @@ from app.api.v1.router import api_router
 from app.models import tutor_subject
 from app.models import booking
 from app.models import review
+from fastapi.middleware.cors import CORSMiddleware
 
 # Crear tablas (Dev only)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Plataforma de Tutorías", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite peticiones desde cualquier origen (incluyendo tu localhost)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 # Aquí incluimos las rutas de usuarios con un prefijo
 app.include_router(api_router, prefix="/api/v1")
